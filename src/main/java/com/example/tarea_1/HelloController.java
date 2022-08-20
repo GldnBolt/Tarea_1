410f024b6bb1791829c297b6eb3ee3fc6814517c
GldnBolt <andblanco@estudiantec.cr>
2022-08-20 17:24:03 -0600

    Creacion de la tabla, columnas y lector del archivo

--- src/main/java/com/example/tarea_1/HelloController.java
+++ src/main/java/com/example/tarea_1/HelloController.java
@@ -1,14 +1,47 @@
 package com.example.tarea_1;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private BufferedReader bufferedReader;
+    private LinkedList linkedList;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onHelloButtonClick()
+    {
+        FileChooser fc = new FileChooser();
+        File document = fc.showOpenDialog(null);
+
+        List<String> Datos = new ArrayList<String>();
+    try {
+        BufferedReader lector = new BufferedReader(new FileReader(document));
+        String divisor = "";
+
+        while ((divisor = lector.readLine()) != null){
+            String[] items = divisor.split(",");
+            for (int i = 0; i < 12; i++){
+                Datos.add(items[i]);
+
+
+            }
+        }
+        System.out.println(Datos);
+    }
+
+    catch (IOException ioException) {
+        ioException.printStackTrace();
+
+    }
+
     }
 }
